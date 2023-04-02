659b7ab9ac8354633d96f4f0d8bc5480f5c3c80c
Amir Ashizhev <amirashizhev@yandex.ru>
2023-04-02 09:44:18 +0300

    done html and css of login wothout validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,38 +3,43 @@ import React from 'react';
 
 function Login () {
   return (
-     <form className="login">
-      <h2 className="login__title">Вход</h2>
-      <fieldset className="login__fieldset">
-        <label className="popup__field">
-          <input 
-            type="email"  
-            name="email" 
-            placeholder="Email" 
-            className="login__input" 
-            pattern="^[^@\s]+@[^@\s]+\.[^@\s]+$"
-            required 
-            // onChange={formValues.handleChange}
-            // value = {formValues.values.email || ""}
-          />
-          <span className="popup__item-error"></span>
-        </label>
-        <label className="popup__field">
-          <input 
-            type="password"  
-            name="password" 
-            placeholder="Пароль" 
-            className="login__input" 
-            required 
-            // onChange={formValues.handleChange}
-            // value = {formValues.values.password || ""}
-            minLength = {3}
-          />
-          <span className="popup__item-error"></span>
-        </label>
-      </fieldset>
-      <button className="login__button" type="submit" aria-label="Войти" >Войти</button>
-    </form>
+    <div className="login">
+      <div className="login__box">
+      <form className="login__form">
+        <h2 className="login__title">Simple Hotel Check</h2>
+        <fieldset className="login__fieldset">
+          <label className="login__field">
+            <span className="login__input-label">Логин</span>
+            <input 
+              type="email"  
+              name="email" 
+              // onChange={formValues.handleChange}
+              // value = {formValues.values.email || ""}
+              className="login__input" 
+              required 
+              pattern="^[^@\s]+@[^@\s]+\.[^@\s]+$"
+              placeholder='login@emal.com'
+            />
+            <span className="login__input-error"></span>
+          </label>
+          <label className="login__field">
+            <span className="login__input-label">Пароль</span>
+              <input 
+                type="password"  
+                name="password" 
+                // onChange={formValues.handleChange}
+                // value = {formValues.values.password || ""}
+                className="login__input"  
+                required 
+                minLength = {8}
+              />
+            <span className="login__input-error"></span>
+          </label>
+        </fieldset>
+        <button className="login__button" type="submit" aria-label="Войти" >Войти</button>
+      </form>
+      </div>
+    </div>
   )
 }
 
