a529d44fbfae386b3320bc304f40d452365aed90
Amir Ashizhev <amirashizhev@yandex.ru>
2023-04-06 14:52:20 +0300

    add hotelList structure and scrollbar

--- src/components/HotelList/HotelList.jsx
+++ src/components/HotelList/HotelList.jsx
@@ -1,9 +1,26 @@
+import Hotel from '../Hotel/Hotel';
 import './HotelList.css'
 
 function HotelList() {
 
   return (
-    <div>
+    <div className='hotelList'>
+      <h3 className='hotelList__title'>Добавлено в Избранное:<span className='hotelList__title-span'>5</span>отелей</h3>
+      <div className='hotelList__countainer'>
+        <ul className='hotelList__items'>
+          <Hotel/>
+          <Hotel/>
+          <Hotel/>
+          <Hotel/>
+          <Hotel/>
+          <Hotel/>      
+          <Hotel/>
+          <Hotel/>
+          <Hotel/>         
+          <Hotel/>
+          <Hotel/>
+        </ul>
+      </div>
 
     </div>
   )
